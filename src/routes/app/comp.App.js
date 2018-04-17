3fb9719953a0f5275bc0082ecc8ebeb5b9a2f6e0
neekey <ni184775761@gmail.com>
2018-04-17 23:58:09 +1000

    update aws secret

--- src/routes/app/comp.App.js
+++ src/routes/app/comp.App.js
@@ -5,7 +5,7 @@ import style from './comp.App.scss';
 export default function App(props) {
   return (<div className={style.container}>
     <div className={style.logo}>
-      Listen PTE
+      Listen PTE.
     </div>
     {props.children}
   </div>);
