03be429857b3b09643aaf0257a1d3c801aac215c
neekey <ni184775761@gmail.com>
2018-04-18 16:33:24 +1000

    update deploy using manual script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+
+base_sha=$(git rev-parse HEAD)
+bucket="s3://listenpte.com"
+region="ap-southeast-2"
+
+# make a copy of index.html
+cp dist/index.html dist/index."$base_sha".html
+cp dist/plans.html dist/plans."$base_sha".html
+
+
+echo "+++ Upload to S3 bucket"
+flags="--region $region --acl public-read"
+aws s3 sync dist $bucket $flags --cache-control "public, max-age=31536000"
+aws s3api put-object --bucket listenpte.com --key index.html --cache-control "public, must-revalidate, proxy-revalidate, max-age=0" --content-type text/html --body dist/index.html
+
+done;
